bf197a78641e0dc64400e9580e44e630865aca6f
me-cafebabe <87921983+me-cafebabe@users.noreply.github.com>
2021-09-20 13:36:38 +0800

    RevengeOS: rova: Create revengeos makefile

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -11,3 +11,11 @@ COMMON_LUNCH_CHOICES := \
     lineage_rova-user \
     lineage_rova-userdebug \
     lineage_rova-eng
+
+PRODUCT_MAKEFILES += \
+    $(LOCAL_DIR)/revengeos_rova.mk
+
+COMMON_LUNCH_CHOICES += \
+    revengeos_rova-user \
+    revengeos_rova-userdebug \
+    revengeos_rova-eng
